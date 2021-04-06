85c9365d9f22db62873b673b5d78b93ddcda4bf9
liuying614 <99758897@qq.com>
2021-04-06 13:10:03 +0800

    测试git

---
+++ src/day02/MyDate.java
@@ -0,0 +1,12 @@
+package day02;
+
+import java.util.Date;
+
+public class MyDate {
+	public static void main(String[] args) {
+		Date da = new Date();
+		int aaa = da.getYear();
+		System.out.println(aaa);
+	}
+
+}
